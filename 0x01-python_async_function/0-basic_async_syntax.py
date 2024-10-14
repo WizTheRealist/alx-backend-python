7d4fde1fa584bfef0d81d3f18e1c80b0e0833844
Chimaobi Ubani <wiztherealist@gmail.com>
2024-10-14 11:24:54 -0700

    async function

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
-
-import random
+"""The basics of async"""
 import asyncio
+from random import random
 
-async def wait_random(max_delay: int =10):
-    x = random.uniform(0, max_delay)
-    if not isinstance (max_delay, int):
-        raise TypeError("Parameter 'max_delay' must be an integer.")
-    await asyncio.sleep(x)
-    return x
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    Write an asynchronous coroutine that takes in an integer argument
+    (max_delay, with a default value of 10) named wait_random that waits
+    for a random delay between 0 and max_delay (included and float value)
+    seconds and eventually returns it.
+
+    Use the random module.
+    """
+    wait_seconds: float = random() * max_delay
+    await asyncio.sleep(wait_seconds)
+    return wait_seconds
--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -14,8 +14,8 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     with the specified max_delay.
 
     wait_n should return the list of all the delays (float values).
-    The list of the delays should be in ascending order without using 
-    sort() because of concurrency.
+    The list of the delays should be in ascending order without using sort()
+    because of concurrency.
     """
     return sorted(
         await gather(
